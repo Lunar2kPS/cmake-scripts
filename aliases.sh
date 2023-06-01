64a9dc5672cc791068adc48d8b08f3c094d2c80e
Carlos DaLomba <carlos@cometpeakgames.net>
2023-06-01 01:06:02 -0400

    Initial commit with CMake/C++ helper scripts!

---
+++ aliases.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ignoreScripts=(
+    "aliases"
+    "get-platform-default-config"
+    "get-platform"
+    "get-project-name"
+)
+
+scriptsFolder=$(dirname $0)
+printf "Adding aliases for the bash scripts under '$scriptsFolder'\n"
+for filePath in "$scriptsFolder/"*; do
+    fileName=$(basename $filePath)
+    aliasName=${fileName/.sh/}
+
+    ignore=false
+    for ignoreName in "${ignoreScripts[@]}"; do
+        if [ "$aliasName" == "$ignoreName" ]; then
+            ignore=true
+            break
+        fi
+    done
+
+    if [ $ignore == true ]; then
+        continue
+    fi
+
+    printf "%-5s%-30s\n" " - " "$aliasName"
+    alias $aliasName="$filePath"
+done
