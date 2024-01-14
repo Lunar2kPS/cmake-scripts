382592b576ed64a7bb79edd797b2ee6dfedbfd5b
Carlos DaLomba <carlos@2kpixelstudios.net>
2024-01-14 01:37:51 -0600

    Added support for multi-level aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,36 +1,80 @@
 #!/bin/bash
 
+currentFolder="$(dirname "${BASH_SOURCE[0]}")"
+proxyAliasFolder=""
 ignoreScripts=(
     "README.md"
     "aliases"
+    "get-main-cmake"
     "get-platform-default-config"
     "get-platform"
     "get-project-name"
-    "get-main-cmake"
+    "get-relative-path"
 )
 
-scriptsFolder="$(dirname "${BASH_SOURCE[0]}")"
-printf "Adding aliases for the bash scripts under '$scriptsFolder'\n"
-for filePath in "$scriptsFolder/"*; do
-    fileName="$(basename $filePath)"
-    if [[ $fileName != *.sh ]]; then
-        continue
+source "$currentFolder/get-relative-path.sh"
+printf "Adding aliases for bash scripts under...\n"
+
+function setDefaultValues() {
+    proxyAliasFolder=""
+    ignoreScripts=()
+}
+
+function addAliasesForCurrentFolder() {
+    local searchFolder=""
+    if [ -n "$proxyAliasFolder" ]; then
+        searchFolder="$proxyAliasFolder"
+    else
+        searchFolder="$currentFolder"
     fi
-
-    aliasName=${fileName/.sh/}
-
-    ignore=false
-    for ignoreName in "${ignoreScripts[@]}"; do
-        if [ "$aliasName" == "$ignoreName" ]; then
-            ignore=true
-            break
+    local displayFolder=""
+    if [ -n "$previousPwd" ]; then
+        displayFolder="$(getRelativePath "$previousPwd" "$searchFolder")"
+    else
+        displayFolder="$searchFolder"
+    fi
+    printf "%2s$displayFolder:\n" ""
+    for filePath in "$searchFolder/"*; do
+        fileName="$(basename $filePath)"
+        if [[ $fileName != *.sh ]]; then
+            continue
         fi
+
+        aliasName=${fileName/.sh/}
+
+        ignore=false
+        for ignoreName in "${ignoreScripts[@]}"; do
+            if [ "$aliasName" == "$ignoreName" ]; then
+                ignore=true
+                break
+            fi
+        done
+
+        if [ $ignore == true ]; then
+            continue
+        fi
+
+        printf "%4s%-4s%-30s\n" "" "-" "$aliasName"
+        alias $aliasName="$filePath"
     done
+}
+addAliasesForCurrentFolder
 
-    if [ $ignore == true ]; then
-        continue
+currentFolder="$(realpath "$(dirname "$currentFolder")")"
+previousPwd="$(realpath "$(pwd)")"
+while [ -n "$currentFolder" ]; do
+    cd "$currentFolder"
+    isInGitRepo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+    cd "$previousPwd"
+    if [ "$isInGitRepo" = true ]; then
+        if [ -f "$currentFolder/aliases.sh" ]; then
+            setDefaultValues
+            source "$currentFolder/aliases.sh"
+            addAliasesForCurrentFolder
+        fi
+    else
+        break
     fi
-
-    printf "%-5s%-30s\n" " - " "$aliasName"
-    alias $aliasName="$filePath"
+    currentFolder="$(realpath "$(dirname $currentFolder)")"
 done
+cd "$previousPwd"
