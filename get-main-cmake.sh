7603eeccdba514bbb04b26f5156ba9c4795f6d69
Carlos DaLomba <carlos@2kpixelstudios.net>
2024-05-26 07:55:49 -0500

    Made get-main.cmake.sh a little bit clearer

--- get-main-cmake.sh
+++ get-main-cmake.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This script checks all the folders between the current folder (pwd) and the folder containing
+# All of the cmake-scripts (not including the cmake-scripts folder),
+# To find the first CMakeLists.txt.
 
 thisScriptFolder="$(dirname "${BASH_SOURCE[0]}")"
 source "$thisScriptFolder/get-platform.sh" --silent
@@ -21,17 +24,17 @@ function checkForCMake() {
     fi
 }
 
-# NOTE: relativePath does NOT have a trailing slash (/), so we'll never the cmake-scripts folder (good!).
+# NOTE: relativePath does NOT have a trailing slash (/), so we'll never check the cmake-scripts folder (good!).
 relativePathLength=${#relativePath}
 for ((i=0; i<$relativePathLength; i++)); do
     currentChar="${relativePath:$i:1}"
-    
+
     # NOTE: printf "-" will trick printf, thinking you're trying to pass an option, and then it'll error out.
     # To make sure this line only treats $currentChar as a string literal, we use %s like so:
     # printf "%s " "$currentChar"
 
     if [[ "$currentChar" = "/" ]]; then
-        nextFolder="${relativePath:$startIndex:$i}"
+        nextFolder="${relativePath:0:$i}"
         checkForCMake "$nextFolder"
     fi
 done
