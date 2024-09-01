5c0745d4be97985510d19756c44238925579740e
Carlos DaLomba <carlos@2kpixelstudios.net>
2024-09-01 01:52:45 -0500

    Added --silent option for initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -5,10 +5,22 @@
 
 # USAGE:
 # (At the start of your scripts)
-# source cmake-scripts/initialize.sh
+# source cmake-scripts/initialize.sh [--silent]
+
+argCount=$#
+args=("$@")
+
+silentMode=false
+for arg in ${args[@]}; do
+    case "$arg" in
+        "--silent")     silentMode=true;;
+    esac
+done
 
 cmakeScriptsFolder="$(dirname "${BASH_SOURCE[0]}")"
 source "$cmakeScriptsFolder/path-utility.sh"
 
 cmakeScriptsFolder="$(getAbsolutePath "$cmakeScriptsFolder")"
-echo "Initialized cmake-scripts folder at: $cmakeScriptsFolder"
+if [ $silentMode != true ]; then
+    echo "Initialized cmake-scripts folder at: $cmakeScriptsFolder"
+fi
