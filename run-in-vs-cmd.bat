702a904b4c706a56711eebc0387f56dc65857f2a
Carlos DaLomba <carlos@2kpixelstudios.net>
2026-03-04 18:18:06 -0600

    Updated run-in-vs-cmd.bat to use VS 2026

--- run-in-vs-cmd.bat
+++ run-in-vs-cmd.bat
@@ -3,4 +3,4 @@
 set "firstArg=%~1"
 @REM echo The first arg is: %firstArg%
 
-"C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvarsall.bat" x64 & %firstArg%
+"C:/Program Files/Microsoft Visual Studio/18/Community/VC/Auxiliary/Build/vcvarsall.bat" x64 & %firstArg%
